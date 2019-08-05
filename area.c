92cad73e1b2d0a8d0f4fe9b7ca13988b4bd6b321
Justine Robert Igune <justine.igune@stud.kiu.ac.ug>
2019-08-05 15:02:45 +0300

    Area of a Circle

---
+++ area.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+##include <conio.h>
+#define pi 3.142
+main()
+{
+  float r, area;
+  printf("enter the radius value\n");
+  scanf("%f", &r);
+  area =pi*r*r;
+  printf("area of a circle =%f, area");
+  return 0
+}
